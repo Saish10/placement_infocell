b638545b8798061796b29efe11024ae276c4bf15
SiddhantNaik17 <siddhantnaik17@gmail.com>
2022-01-15 11:44:39 +0530

    accounts: Add student profile details

--- accounts/models.py
+++ accounts/models.py
@@ -5,7 +5,19 @@ from accounts.manager import UserManager
 
 
 class User(AbstractBaseUser):
+    class Status(models.TextChoices):
+        PLACED = 'pl', 'Placed'
+        UNPLACED = 'up', 'Unplaced'
+
+    name = models.CharField(max_length=50)
     email = models.EmailField(max_length=254, unique=True)
+    phone = models.CharField(max_length=13, blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.UNPLACED)
+    aggregate = models.PositiveSmallIntegerField(blank=True, null=True)
+    department = models.ForeignKey('departments.Department', blank=True, null=True,
+                                   on_delete=models.CASCADE, related_name='students')
+
     is_admin = models.BooleanField(default=False)
     is_active = models.BooleanField(default=False)
     updated_at = models.DateTimeField(auto_now=True)
